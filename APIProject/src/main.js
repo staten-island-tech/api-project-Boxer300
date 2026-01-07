80cef99ec9bd64adaa37bfe8db08526923dab6fb
Boxer300 <romano2789@nycstudents.net>
2026-01-07 12:30:33 -0500

    Update main.js

--- APIProject/src/main.js
+++ APIProject/src/main.js
@@ -1,15 +1,16 @@
 import './style.css'
-async function getAllData(Disney){
+const URL = "https://api.disneyapi.dev/character/:id"
+async function getAllData(URL){
  try {
     // go get data
-    const response = await fetch(`https://api.disneyapi.dev/character`);
+    const response = await fetch(URL);
     // handle errors
     if (response.status != 200) {
       throw new Error(response);
     } else {
       // makes the response into json data we can use
       const data = await response.json();
-      console.log(data);
+      const response = await fetch(URL);
       document.getElementById("api-response").textContent = data.name;
     }
   } catch (error) {
@@ -17,19 +18,6 @@ async function getAllData(Disney){
   }
 }
 getAllData()
-const info = `https://api.disneyapi.dev/character`
- info.forEach((data) => inject(data))
- function inject(info) {
-    document.querySelector(".main").insertAdjacentHTML(
-    "afterbegin",
-     `<div class="display-card">
-       <img class="display-image" src="${info.imageUrl}" />
-        <input class="caption" placeholder="meme text" />
-        <h2 class="display-category">${info.name} </h2>
-      <h3 class="display-title">${info.category} </h3>
-      <button class="add text">Add text</button>
-   </div>`
-    );
-   }  
 
 /* https://api.disneyapi.dev/character/:id */
+/* https://api.disneyapi.dev/character */
